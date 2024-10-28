59fdea15a34626372e67ac2ef56c0f3c3f55d5a7
Monika Stracenski <62952373+monikastrac@users.noreply.github.com>
2024-10-28 06:16:43 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD3GNfnaLewTr8Ead21HP4X2BMG5Duw_T4",
+    apiKey: "...",
     authDomain: "app1monika.firebaseapp.com",
     projectId: "app1monika",
     storageBucket: "app1monika.appspot.com",
-    messagingSenderId: "752860530783",
-    appId: "1:752860530783:web:b51ad817707282835233ea",
-    measurementId: "G-F65WCNJTF7"
+    messagingSenderId: "...",
+    appId: "1::web:",
+    measurementId: "G-..."
   };
   
 const app = firebase.initializeApp(firebaseConfig);
